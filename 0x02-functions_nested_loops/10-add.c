0408c387f5aefd540fe54c07bc957877d4e7d628
Maameadiefe <maameadiefe@gmail.com>
2023-04-26 18:14:13 -0700

    Adiefe is on 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Addition of two numbers
+ * @n1: First number to compute
+ * @n2: Second number to compute
+ * Return: Integer value
+ */
+
+int add(int n1, int n2)
+
+{
+
+	return (n1 + n2);
+
+}
