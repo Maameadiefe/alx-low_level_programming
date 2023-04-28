3bab60ac9fcbdc4fd60615d2d6b749ef60d7d0d0
Maameadiefe <maameadiefe@gmail.com>
2023-04-28 05:12:19 -0700

    Adiefe on 1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isdigit - Chests if a charater is a digit
+ * @y : The charater to check
+ * Return: 1 if the character is a digit else 0
+ */
+
+int _isdigit(int y)
+
+{
+	if (y >= 48 && y <= 57)
+	{
+	return (1);
+	}
+	return (0);
+}
