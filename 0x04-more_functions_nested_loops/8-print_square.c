f9389cf4a7ffdc37aa9104fe1ca3c284825d2342
Maameadiefe <maameadiefe@gmail.com>
2023-04-28 08:27:10 -0700

    Adiefe is on 8-print_square.c

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_square - Printsquares according to n parameter
+ * @n: The number of times to print
+ * Return: empty
+ */
+
+void print_square(int n)
+
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+	_putchar('\n);
+	}
+	else
+	{
+	for (x = 0; x < n; x++)
+	{
+	for (y = 0; y < n; y++)
+	{
+	_putchar(35);
+	}
+	_putchar('\n');
+	}
+	}
+}
