9cdfe49d94d36f141b9da830d547d644bd592ec2
Maameadiefe <maameadiefe@gmail.com>
2023-04-28 04:55:50 -0700

    Adiefe on 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks whether a letter is uppercase
+ * @y: The number to be checked
+ * Return: 1 for upper and 0 for anything else
+ */
+
+int _isupper(int y)
+
+{
+	if (y >= 65 && y <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
