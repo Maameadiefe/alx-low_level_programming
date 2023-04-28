557e871f420ecfffcf472c25a527cada576719ed
Maameadiefe <maameadiefe@gmail.com>
2023-04-28 05:25:56 -0700

    Adiefe on 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * mul - Multiplying two integers
+ * @x: The first interger of the expression
+ * @z: The second integer of the expression
+ * Return: The result from the multplication of the two integers
+ */
+
+int mul(int x int z)
+
+{
+	return (x * z);
+}
