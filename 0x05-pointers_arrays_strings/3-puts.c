1b385b53cc7bd522bcb1996d2a5e14d63297e975
Maameadiefe <maameadiefe@gmail.com>
2023-05-03 03:27:43 -0700

    Adiefe on 3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,14 +2,14 @@
 
 /**
  * _puts - prints a string and followed by a new line to stdout
- * @str: string to print
+ * @str: string to be print
  */
 
 void _puts(char *str)
 {
 	while (*str != '\0')
 	{
-		_putchar(*str);
+		_putchar(*str++);
 	}
 		_putchar('\n');
 }
