db715aa5ecfa916a77ccbbaebb9be05852628cfa
maamediefe <maameadiefe@gmail.com>
2023-05-10 15:54:19 -0700

    Adiefe wrote a function that raises x to the power y

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - This function returns the value of x ^ y
+ * @x: the value to have a power of y
+ * @y: the power of the integer
+ * Return: x ^ y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else 
+		if (y == 0)
+			return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
