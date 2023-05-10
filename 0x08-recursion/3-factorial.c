186e644fc250b9770b69fed0e73eee2de7ab16e4
maamediefe <maameadiefe@gmail.com>
2023-05-10 12:23:02 -0700

    Adiefe is on factorial task

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * factorial - This untion returns the factorialof a given number
+ * @x: the given number
+ * Return: Value
+ */
+int factorial(int n)
+{
+	if (x <= 1)
+		return (1);
+	else
+		return (x * factorial(x - 1));
+}
