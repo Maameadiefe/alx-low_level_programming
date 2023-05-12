61a8ed2c69a6e1a02ca75cb513803c178cf307fb
maamediefe <maameadiefe@gmail.com>
2023-05-12 12:21:07 -0700

    Adiefe is on 1-memcpy.c

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - this function copies memory area
+ * @dest: mem where stored
+ * @src: mem where copied
+ * @n: bytes
+ * Return: copied mem with n bytes
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int x = 0;
+	int i = 0;
+
+	for (; x < i; x++)
+	{
+		dest[x] = src[x];
+		n--;
+	}
+	return (dest);
+}
