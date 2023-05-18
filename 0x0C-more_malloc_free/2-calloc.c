549987b46eb2e59ca921077837737daef90f4fb8
maamediefe <maameadiefe@gmail.com>
2023-05-18 15:45:26 -0700

    Adiefe is on 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _calloc - this function allocates memory for an array
+ * @nmemb: number of element
+ * @size: bytes of each
+ * Return: NULL
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *x;
+	unsigned int i;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	x = malloc(nmemb * size);
+	if (x == NULL)
+		return (NULL);
+	for (i = 0; i < nmemb * size; i++)
+		x[i] = 0;
+	return (x);
+}
