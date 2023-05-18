7e74d45ecbd5bf6be4e5e8c914dec0bd4df74223
maamediefe <maameadiefe@gmail.com>
2023-05-18 16:18:32 -0700

    Adiefe is on 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,50 +2,43 @@
 #include <stdlib.h>
 
 /**
- * string_nconcat - this funtion concatenates two strings
- * @s1: first string for concatenation
- * @s2: second string for concatenation
- * @n: bytes
- * Return: Nothing
+ * string_nconcat - two strings are concatenated
+ * @s1: first string
+ * @s2: second string
+ * @n: index
+ * Return: NULL
  */
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int x = 0, y = 0, z = 0, w = 0;
-	char *str;
+	char *s;
+	unsigned int i = 0, y = 0, z;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-
-	while (s2[x])
-		x++;
-	
-	while (s2[z])
-		z++;
-
-	if (n >= z)
-		w = x + z;
-	else
-		w = x + n;
-
-	str = malloc(sizeof(char) * w + 1);
-	if ( str == NULL)
-		return (NULL);
-
-	z = 0;
-	while (y < w)
+	while (s1[i] != '\0')
 	{
-		if (y <= x)
-			str[y] = s1[y];
-
-		if (y >= x)
-		{
-			str[y] = s2[z];
-			z++;
-		}
+		i++;
+	}
+	while (s2[y] != '\0')
+	{
 		y++;
 	}
-	str[y] = '\0';
-	return (str);
+	if (n > y)
+		n = y;
+	s = malloc((i + n + 1) * sizeof(char));
+	if (s == NULL)
+		return (0);
+	for (z = 0; z < i; z++)
+	{
+		s[z] = s1[z];
+	}
+	for (; z < (i + n); z++)
+	{
+		s[z] = s2[z - i];
+	}
+	s[z] = '\0';
+	return (s);
 }
