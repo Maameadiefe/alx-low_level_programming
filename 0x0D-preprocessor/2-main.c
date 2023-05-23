213c3aee43d9b07ca2a1ec43b0b1bd434fe70a57
maamediefe <maameadiefe@gmail.com>
2023-05-22 17:23:08 -0700

    Adiefe is on 3-function_like_macro.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this functions prints the name of a file
+ * FILE: name to print
+ * Return: Nothing
+ */
+
+int main(void)
+{
+	printf("%s\n", FILE);
+	return (0);
+}
