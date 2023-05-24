6caec3a43982e01c97e7634d88a0ee3c24d71020
maamediefe <maameadiefe@gmail.com>
2023-05-23 22:36:29 -0700

    Adiefe is on dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,6 +8,7 @@
  * @owner: owner of the dog
  */
 
+
 struct dog
 {
 	char *name;
@@ -15,13 +16,9 @@ struct dog
 	char *owner;
 };
 
-/**
- * dog_t - this is the typedef for struct dog.
- */
-typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+typedef struct dog dog_t;
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 char *_strcpy(char *dest, char *src);
