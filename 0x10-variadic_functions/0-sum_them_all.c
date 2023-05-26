4abd46dffaa269d58e919782a3a5c9cd4634007c
maamediefe <maameadiefe@gmail.com>
2023-05-26 03:26:26 -0700

    Adiefe is on task 0 again

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -9,14 +9,13 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
+	va_list adf;
+	unsigned int x, sum = 0;
+
 	if  (n == 0)
 	{
 		return (0);
 	}
-
-	va_list adf;
-	unsigned int x, sum = 0;
-
 	va_start(adf, n);
 	for (x = 0; x < n; x++)
 		sum += va_arg(adf, int);
