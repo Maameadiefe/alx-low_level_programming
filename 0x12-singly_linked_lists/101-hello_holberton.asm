8882d041dd4b7e709314badd605d8b88327fcc4c
Maameadiefe <maameadiefe@gmail.com>
2023-06-06 09:58:28 -0700

    Adiefe is on it

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -7,15 +7,15 @@ section .text
 
 global main
 main:
-    sub rsp, 8                  ; Align the stack
+    sub rsp, 8                     ; Align the stack
 
-    mov rdi, fmt               ; Set the address of the format string
-    mov rsi, msg               ; Set the address of the hello string
-    xor eax, eax               ; Clear the eax register
-    call printf                ; Call the printf function
+    mov rdi, fmt                  ; Set the address of the format string
+    mov rsi, msg                  ; Set the address of the hello string
+    xor eax, eax                  ; Clear the eax register
+    call printf                   ; Call the printf function
 
-    add rsp, 8                 ; Restore the stack
-    mov eax, 60                ; Exit system call number
-    xor edi, edi               ; Exit status 0
-    syscall                    ; Invoke the system call
+    add rsp, 8                    ; Restore the stack
+    xor edi, edi                  ; Clear the edi register for the exit status
+    mov eax, 60                   ; Exit system call number
+    syscall                       ; Invoke the system call
 
