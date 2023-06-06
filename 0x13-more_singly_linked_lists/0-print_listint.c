8dac25089c9a1f0a79eab7b40a07312105ba2f5f
Maameadiefe <maameadiefe@gmail.com>
2023-06-06 10:17:10 -0700

    Adiefe is on 0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - displays the elements of a listint_t list
+ * @h: a pointer to the head of the listint_t list
+ * Return: the number of nodes in the listint_t list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t adf = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		adf++;
+	}
+
+	return (adf);
+}
+
