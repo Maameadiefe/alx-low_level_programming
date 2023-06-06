e5d4facd5972e5020e89c3bc9eb39f805c450b1b
Maameadiefe <maameadiefe@gmail.com>
2023-06-06 09:53:43 -0700

    Adiefe is on it

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,24 +1,21 @@
 section .data
     msg db "Hello, Holberton", 0
-    fmt db "%s,", 10, 0
+    fmt db "%s", 10, 0
 
 section .text
     extern printf
 
 global main
 main:
-    sub rsp, 8                     ; Align the stack
+    sub rsp, 8                  ; Align the stack
 
-    mov rdi, fmt                  ; Set the address of the format string
-    mov rsi, msg                  ; Set the address of the hello string
-    xor rax, rax                  ; Clear the rax register
-    call printf                   ; Call the printf function
+    mov rdi, fmt               ; Set the address of the format string
+    mov rsi, msg               ; Set the address of the hello string
+    xor eax, eax               ; Clear the eax register
+    call printf                ; Call the printf function
 
-    add rsp, 8                    ; Restore the stack
-    mov eax, 60                   ; Exit system call number
-    xor edi, edi                  ; Exit status 0
-    syscall                       ; Invoke the system call
-
-section .bss
-    ; Declare variables or uninitialized data here if needed
+    add rsp, 8                 ; Restore the stack
+    mov eax, 60                ; Exit system call number
+    xor edi, edi               ; Exit status 0
+    syscall                    ; Invoke the system call
 
